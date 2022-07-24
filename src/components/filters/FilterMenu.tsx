4161978169c4c6e6a105117922eb35e4933b56a8
Cristiano Faustino <cristiadu@gmail.com>
2022-07-24 00:53:38 -0700

    adding burger sliding menu

--- src/components/filters/FilterMenu.tsx
+++ src/components/filters/FilterMenu.tsx
@@ -1,8 +1,34 @@
 import React from 'react'
-import { Box } from 'react-bulma-components'
+import { Block } from 'react-bulma-components'
+import { slide as SlideMenu } from 'react-burger-menu'
+
+const styles = {
+  bmBurgerButton: {
+    position: 'absolute',
+    width: '25px',
+    height: '18px',
+    left: '16px',
+    top: '16px',
+  },
+  bmBurgerBars: {
+    background: '#373a47',
+  },
+  bmBurgerBarsHover: {
+    background: '#a90000',
+  },
+  bmCrossButton: {
+    height: '744px',
+    width: '744px',
+  },
+  bmCross: {
+    background: '#373a47',
+  },
+}
 
 export default () => (
-  <Box className="has-background-danger-light" mb={1} style={{ height: '99%' }}>
-    Filters &gt;&gt;
-  </Box>
+  <SlideMenu className="box has-background-danger-light" styles={styles} noOverlay>
+    <Block>
+      Filters
+    </Block>
+  </SlideMenu>
 )
