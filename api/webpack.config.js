540a08ecf36954267a80d1a269472f174cf47f47
Cristiano Faustino <cristiadu@gmail.com>
2024-04-09 00:43:09 -0700

    Heroku test for API (#454)
    * adding firebase
    
    * adding firebase
    
    * removing secret
    
    * removing secret
    
    * removing secret
    
    * firebase needs paid version to work properly
    
    * proper triggers

--- api/webpack.config.js
+++ api/webpack.config.js
@@ -1,40 +1,45 @@
 const path = require("path");
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 // const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = {
-  target: 'node', // This is important for backend development
-  entry: "./index.ts", // Entry point of your application
-  plugins: [
-    // new BundleAnalyzerPlugin(),
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: '../public/images', to: 'images' },
-        { from: '../public/manifest.json', to: 'manifest.json' },
-        { from: '../public/robots.txt', to: 'robots.txt' },
-        { from: '../public/favicon.ico', to: 'favicon.ico' },
-      ]
-    }),
-  ],
-  output: {
-    filename: "bundle.js", // Output bundle file name
-    path: path.resolve(__dirname, "../public-api"), // Output directory
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|ts)$/,
-        exclude: [/node_modules/, /src\/test\//],
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [["@babel/preset-env", { modules: false }], "@babel/preset-typescript"],
-          },
-        },
-      },
+    target: 'node', // This is important for backend development
+    entry: "./index.ts", // Entry point of your application
+    plugins: [
+        // new BundleAnalyzerPlugin(),
+        new HtmlWebpackPlugin({
+
+            template: 'public/index.html'
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: '../public/images', to: 'images' },
+                { from: '../public/manifest.json', to: 'manifest.json' },
+                { from: '../public/robots.txt', to: 'robots.txt' },
+                { from: '../public/favicon.ico', to: 'favicon.ico' },
+            ]
+        }),
     ],
-  },
-  resolve: {
-    extensions: [".js", ".ts"],
-  },
+    output: {
+        filename: "bundle.js", // Output bundle file name
+        path: path.resolve(__dirname, "dist"), // Output directory
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|ts)$/,
+                exclude: [/node_modules/, /src\/test\//],
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [["@babel/preset-env", { modules: false }], "@babel/preset-typescript"],
+                    },
+                },
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".js", ".ts"],
+    },
 }
