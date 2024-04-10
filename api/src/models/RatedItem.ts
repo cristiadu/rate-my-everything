590e11b10bc5c875a2aac3e342440e879dd1fce4
Cristiano Faustino <cristiadu@gmail.com>
2024-04-09 23:46:43 -0700

    fixing validations

--- api/src/models/RatedItem.ts
+++ api/src/models/RatedItem.ts
@@ -9,7 +9,7 @@ export default class RatedItem {
     @Column()
       item_id!: number
 
-    @Column({ type: 'decimal', precision: 2, scale: 2 })
+    @Column({ type: 'decimal', precision: 4, scale: 2 })
     @Min(0.0)
     @Max(10.0)
       rating!: number
