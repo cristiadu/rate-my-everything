28dda3d5d74a38188390368ebbd037448aa75c42
Cristiano Faustino <cristiadu@gmail.com>
2024-12-09 19:56:41 -0800

    fix test

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,2 +1,8 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
+
+// Polyfill for TextEncoder and TextDecoder
+import { TextEncoder, TextDecoder } from 'util';
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder as typeof global.TextDecoder;
